9e26924a4fa9b15aa23fdadf14fd495aca817448
giobasiliox <125027610+giobasiliox@users.noreply.github.com>
2024-05-06 09:27:39 -0300

    feat: add rota para buscar herói por id

--- index.js
+++ index.js
@@ -43,6 +43,18 @@ app.get('/herois', async (req, res) => {
     }
 });
 
+app.get('/heroi/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await pool.query('SELECT * FROM herois WHERE id = $1', [id]);
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error('Erro ao buscar herói:', error);
+        res.status(500).send('Erro ao buscar herói');
+    }
+});
+
 
 
 
